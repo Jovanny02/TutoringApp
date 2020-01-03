9abe100d68edf7d680b0a191750c86dddadd29ad
DESKTOP-OM9N3U2\Main PC <man-0-war@hotmail.com>
2020-01-02 20:13:50 -0500

    Add project files.

---
+++ ResumeApp/ResumeApp/Models/NavigationTile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResumeApp.Models
+{
+    class NavigationTile
+    {
+        public string pageName { get; set; }
+        public string iconSrc { get; set; }
+        public Type targetType { get; set; }
+
+    }
+}
