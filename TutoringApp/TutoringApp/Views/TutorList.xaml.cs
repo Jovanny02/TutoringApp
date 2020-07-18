f58e158b86d395576585b1c121e472593f8d8f8d
Jovanny Vera <jovannyvera@ufl.edu>
2020-07-18 17:13:23 -0400

    -Test Commit

---
+++ TutoringApp/TutoringApp/Views/TutorList.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TutoringApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TutorList : ContentPage
+    {
+        public TutorList()
+        {
+            InitializeComponent();
+        }
+    }
+}
