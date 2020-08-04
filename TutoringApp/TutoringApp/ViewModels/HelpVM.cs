cd1eca93e65ed8166fa9b0c172ce7347108f0355
Kareem Joudeh <kjoudeh@ufl.edu>
2020-08-04 11:32:54 -0400

    added help page code

--- TutoringApp/TutoringApp/ViewModels/HelpVM.cs
+++ TutoringApp/TutoringApp/ViewModels/HelpVM.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using System.Windows.Input;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using TutoringApp.Views;
+using System.Diagnostics;
+using Xamarin.Essentials;
 
 namespace TutoringApp.ViewModels
 {
     class HelpVM
     {
+        private Boolean IsValidUri(String uri)
+        {
+            try
+            {
+                new Uri(uri);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public ICommand ClickCommand => new Command<string>(async (url) =>
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(url))
+                {
+                    if (!url.Trim().StartsWith("http", StringComparison.OrdinalIgnoreCase))
+                    {
+                        url = "http://" + url;
+                    }
+                    if (IsValidUri(url))
+                    {
+                        await Browser.OpenAsync(new Uri(url), BrowserLaunchMode.SystemPreferred);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+        });
     }
 }
