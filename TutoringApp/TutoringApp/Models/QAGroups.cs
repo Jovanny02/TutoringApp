bcaa2154b6f4b2401f10f934b3a81565a7218e0a
Kareem Joudeh <kjoudeh@ufl.edu>
2020-08-09 19:58:08 -0400

    add list in helpVM

---
+++ TutoringApp/TutoringApp/Models/QAGroups.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutoringApp.Models { 
+
+    public class QAGroups : List<QA>
+    {
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+
+        public QAGroups(string title, string shortTitle)
+        {
+            Title = title;
+            ShortTitle = shortTitle;
+
+        }
+    }
+}
--- TutoringApp/TutoringApp/ViewModels/HelpVM.cs
+++ TutoringApp/TutoringApp/ViewModels/HelpVM.cs
@@ -7,45 +7,73 @@ using Xamarin.Forms;
 using TutoringApp.Views;
 using System.Diagnostics;
 using Xamarin.Essentials;
+using TutoringApp.Models;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace TutoringApp.ViewModels
 {
-    class HelpVM
-    {
-        private Boolean IsValidUri(String uri)
-        {
-            try
-            {
-                new Uri(uri);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
+    class HelpVM 
 
-        public ICommand ClickCommand => new Command<string>(async (url) =>
-        {
-            try
+    {
+       public List<QAGroups> QAview = new List<QAGroups>
+                    {
+                        new QAGroups("FAQ Student", "FAQS")
+                            {
+                                 new QA { Questions = "Do i need to be a student to be a student at UF to use the app", Answers = "Yes these course are only available at UF and this app only support the university" }
+                            },
+
+
+                        new QAGroups("FAQ tutors ", "FAQT")
+                        {
+                            new QA { Questions = "Will i get refund if i cancel a session", Answers = "No, if u cancel a charge in your own violation you will not get payed" }
+                        }
+
+
+                    };
+        
+
+            
+
+            private Boolean IsValidUri(String uri)
             {
-                if (!string.IsNullOrEmpty(url))
+                try
                 {
-                    if (!url.Trim().StartsWith("http", StringComparison.OrdinalIgnoreCase))
-                    {
-                        url = "http://" + url;
-                    }
-                    if (IsValidUri(url))
-                    {
-                        await Browser.OpenAsync(new Uri(url), BrowserLaunchMode.SystemPreferred);
-                    }
+                    new Uri(uri);
+                    return true;
+                }
+                catch
+                {
+                    return false;
                 }
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
 
-        });
-    }
-}
+
+            public ICommand ClickCommand => new Command<string>(async (url) =>
+            {
+                try
+                {
+                    if (!string.IsNullOrEmpty(url))
+                    {
+                        if (!url.Trim().StartsWith("http", StringComparison.OrdinalIgnoreCase))
+                        {
+                            url = "http://" + url;
+                        }
+                        if (IsValidUri(url))
+                        {
+                            await Browser.OpenAsync(new Uri(url), BrowserLaunchMode.SystemPreferred);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+
+            });
+
+
+         }
+
+   }
+      
+
