374bfcc18608584abcd7ee38364da70a21bf1e68
Jovanny Vera <jovannyvera@ufl.edu>
2020-12-01 01:09:11 -0500

    -Added set reservations API call -Added API call to get selected tutors reservations and remove them from the list that can be selected from when a student is selectinga new session

--- AppWebAPI/Models/LoginModel.Context.cs
+++ AppWebAPI/Models/LoginModel.Context.cs
@@ -28,8 +28,8 @@ namespace AppWebAPI.Models
         public virtual DbSet<loginTest> loginTests { get; set; }
         public virtual DbSet<user> users { get; set; }
         public virtual DbSet<reservation> reservations { get; set; }
-        public virtual DbSet<Cours> Courses { get; set; }
         public virtual DbSet<EducationSection> EducationSections { get; set; }
         public virtual DbSet<userSchedule> userSchedules { get; set; }
+        public virtual DbSet<Cours> Courses { get; set; }
     }
 }
