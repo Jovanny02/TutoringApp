85dac30bd69684e1c2cd31feb8573836b805a055
Jovanny Vera <jovannyvera@ufl.edu>
2020-12-06 00:00:31 -0500

    merged

--- TutoringApp/TutoringApp/ViewModels/studentReservationListVM.cs
+++ TutoringApp/TutoringApp/ViewModels/studentReservationListVM.cs
@@ -9,57 +9,70 @@ using Xamarin.Forms;
 using Acr.UserDialogs;
 //using System.Text.Json;
 using System.Threading.Tasks;
+using TutoringApp.Services;
+using System.Text.Json;
 
 namespace TutoringApp.ViewModels
 {
     public class studentReservationListVM : BaseVM
     {
-        public studentReservationListVM() { 
+        public studentReservationListVM() {
+            User student = new User();
+            PerformReservationCommand.Execute(student);
+        }
         //TODO Add API call to get  reservations for current user
-            studentSessions.Add(new ReservationTile
+        public ICommand PerformReservationCommand => new Command<User>(async (student) =>
+        {
+           
+            student = JsonSerializer.Deserialize<User>(App.Current.Properties["CurrentUser"] as string);
+            List<ReservationTile> studentReservation = await WebAPIServices.getStudentReservations(student.UFID);
+            if (studentReservation != null)
             {
-                tutorName = "Test Tutor ",
-                studentName = "Test Student ",
-                zoomLink = "https://zoom.us/",
-                fromDate = DateTime.Now.AddHours(-4),
-                toDate = DateTime.Now.AddHours(-3),
-                tutorUFID = 12345678,
-                studentUFID = 87654321,
-                isCompleted = true
-            });
-
-            for (int i = 0; i < 5; i++)
+                studentReserve = new ObservableCollection<ReservationTile> (studentReservation);
+            }
+            else
             {
-                studentSessions.Add(new ReservationTile
-                {
-                    tutorName = "Test Tutor " + i,
-                    studentName = "Test Student " + i,
-                    zoomLink = "https://zoom.us/",
-                    fromDate = DateTime.Now.AddHours(i - 3),
-                    toDate = DateTime.Now.AddHours(i - 2),
-                    tutorUFID = 12345678,
-                    studentUFID = 87654321
-                });
-
+                studentReserve = null;
             }
 
+            //properties changed
+            onPropertyChanged(nameof(studentReserve));
+          onPropertyChanged(nameof(isStudentsVisible));
+        });
+
+
+        public ObservableCollection<ReservationTile> studentReserve { get; set; } = new ObservableCollection<ReservationTile>();
+        
+       // public ObservableCollection<ReservationTile> studentSessions { get; set; } = new ObservableCollection<ReservationTile>();
+
+        //public bool isStudentsVisible { get { return (studentSessions.Count > 0); } }
+
+        public bool isStudentsVisible
+        {
+            get
+            {
+                if (studentReserve == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return studentReserve.Count > 0;
+                }
+            }
         }
-
-        public ObservableCollection<ReservationTile> studentSessions { get; set; } = new ObservableCollection<ReservationTile>();
-
-        public bool isStudentsVisible { get { return (studentSessions.Count > 0); } }
 
         public ICommand studentReservationCommand => new Command<object>((selectedItem) =>
         {
-            int selectedItemIndex = studentSessions.IndexOf((ReservationTile)selectedItem);
+            int selectedItemIndex = studentReserve.IndexOf((ReservationTile)selectedItem);
 
             bool showSubmit = false;
             //if the reservation is not cancelled, not complete, and the end time has passed, allow the showing of the submission prompt
-            if (!studentSessions[selectedItemIndex].isCanceled && !studentSessions[selectedItemIndex].isCompleted && studentSessions[selectedItemIndex].toDate < DateTime.Now)
+            if (!studentReserve[selectedItemIndex].isCanceled && !studentReserve[selectedItemIndex].isCompleted && studentReserve[selectedItemIndex].toDate < DateTime.Now)
                 showSubmit = true;
 
-            ReservationDetails reservationDetails = new ReservationDetails(true, showSubmit, studentSessions[selectedItemIndex].statusMessage);
-            reservationDetails.BindingContext = studentSessions[selectedItemIndex];
+            ReservationDetails reservationDetails = new ReservationDetails(true, showSubmit, studentReserve[selectedItemIndex].statusMessage);
+            reservationDetails.BindingContext = studentReserve[selectedItemIndex];
             reservationDetails.submitRatingCommand = submitRatingCommand;
             Navigation.PushAsync(reservationDetails);
 
@@ -70,7 +83,7 @@ namespace TutoringApp.ViewModels
         {
             try
             {
-                int indexOfReservation = studentSessions.IndexOf((ReservationTile)selectedReservation);
+                int indexOfReservation = studentReserve.IndexOf((ReservationTile)selectedReservation);
 
 
                 //TODO ADD API CALL TO SUBMIT RATING
@@ -81,8 +94,8 @@ namespace TutoringApp.ViewModels
 
 
                 //update reservation on users end immediately
-                studentSessions[indexOfReservation].isCompleted = true;
-                onPropertyChanged(nameof(studentSessions));
+                studentReserve[indexOfReservation].isCompleted = true;
+                onPropertyChanged(nameof(studentReserve));
                 await Navigation.PopAsync();
             }
             catch (Exception e)
